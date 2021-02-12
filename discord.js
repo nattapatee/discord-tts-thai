0484552e95c0fda6c690f656df18ff250fcb40d4
nattapatee <nattapat.ek@bcircle.co.th>
2021-02-13 02:53:10 +0700

    add en speak and help & heroku

--- discord.js
+++ discord.js
@@ -11,31 +11,76 @@ client.on("ready", () => {
 });
 
 client.on("message", msg => {
-    if (msg.content.includes("/s ")) {
-        let text = msg.content.split(" ")
-        if(text.length == 0) {
-            msg.reply('กรุณาพิมพ์ให้ถูกอีสัส');
-        } else {
-        text.shift()
+    if (msg.content.includes("เปิดไมค์")) {
         const broadcast = client.voice.createBroadcast();
         var channelId = msg.member.voice.channelID;
         var channel = client.channels.cache.get(channelId);
         channel.join().then(connection => {
-            broadcast.play(discordTTS.getVoiceStream(text.join(" "), 'th-TH'), {
-                volume: 0.15,
+            broadcast.play(discordTTS.getVoiceStream("ได้ยินกูไหมหละ", 'th-TH'), {
+                volume: 0.2,
             });
             const dispatcher = connection.play(broadcast);
-            // const stream = discordTTS.getVoiceStream("banana ทดสอบ", 'th-TH')
-            // broadcast.play(stream);
-            // const dispatcher = await connection.play(broadcast)
-        }).catch(err => {
-            log('error', `description: ${err}`);
+            return;
 
-        });
+        }).catch(err => {
+            log('error', `Description: ${err}`);
+        })
+    }
+
+    if (msg.content.includes("/พ ")) {
+        let text = msg.content.split(" ")
+        if (text.length == 0) {
+            msg.reply('กรุณาพิมพ์ให้ถูกอีสัส');
+        } else {
+            text.shift()
+            const broadcast = client.voice.createBroadcast();
+            var channelId = msg.member.voice.channelID;
+            var channel = client.channels.cache.get(channelId);
+            channel.join().then(connection => {
+                broadcast.play(discordTTS.getVoiceStream(text.join(" "), 'th-TH'), {
+                    volume: 0.2,
+                });
+                const dispatcher = connection.play(broadcast);
+                log('info', `Speaking: ${text.join(" ")}`);
+                msg.reply('เดี๋ยวพูดให้นะจ๊ะ');
+                return;
+
+            }).catch(err => {
+                log('error', `Description: ${err}`);
+            });
         }
     }
+
+    if (msg.content.includes("/s ")) {
+        let text = msg.content.split(" ")
+        if (text.length == 0) {
+            msg.reply('กรุณาพิมพ์ให้ถูกอีสัส');
+        } else {
+            text.shift()
+            const broadcast = client.voice.createBroadcast();
+            var channelId = msg.member.voice.channelID;
+            var channel = client.channels.cache.get(channelId);
+            channel.join().then(connection => {
+                broadcast.play(discordTTS.getVoiceStream(text.join(" ")), {
+                    volume: 0.2,
+                });
+                const dispatcher = connection.play(broadcast);
+                msg.reply('I\'ll say it for you.');
+                log('info', `Speaking: ${text.join(" ")}`);
+                return;
+            }).catch(err => {
+                log('error', `Description: ${err}`);
+            });
+        }
+    }
+
+    if (msg.content.includes("/helpSpeak")) {
+        msg.channel.send('Slowpoke จะเป็นปากให้พวกมึงเองวิธีใช้งาน ```/พ``` สำหรับภาษาไทย ```/s``` สำหรับภาษาอังกฤษ')
+    }
+
     if (msg.content === "อี๊") {
         msg.reply('มีไรไอ้อ้วน');
+        return;
     }
 
 });
