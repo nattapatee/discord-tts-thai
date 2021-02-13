83c84eb9cc7fbeb2b94d63dfcc3b3aa62162bcfd
nattapatee <nattapat.ek@bcircle.co.th>
2021-02-13 13:29:49 +0700

    add language

--- discord.js
+++ discord.js
@@ -27,7 +27,33 @@ client.on("message", msg => {
         })
     }
 
+    if (msg.content.includes("/j ")) {
+        log('info', `กำลังรับข้อความ JAPAN`);
+        let text = msg.content.split(" ")
+        if (text.length == 0) {
+            msg.reply('กรุณาพิมพ์ให้ถูกอีสัส');
+        } else {
+            text.shift()
+            const broadcast = client.voice.createBroadcast();
+            var channelId = msg.member.voice.channelID;
+            var channel = client.channels.cache.get(channelId);
+            channel.join().then(connection => {
+                broadcast.play(discordTTS.getVoiceStream(text.join(" "), 'ja-JP'), {
+                    volume: 0.2,
+                });
+                const dispatcher = connection.play(broadcast);
+                log('info', `Speaking: ${text.join(" ")}`);
+                msg.reply('私はあなたのためにそれを言います');
+                return;
+
+            }).catch(err => {
+                log('error', `Description: ${err}`);
+            });
+        }
+    }
+
     if (msg.content.includes("/พ ")) {
+        log('info', `กำลังรับข้อความ THAI`);
         let text = msg.content.split(" ")
         if (text.length == 0) {
             msg.reply('กรุณาพิมพ์ให้ถูกอีสัส');
@@ -51,7 +77,84 @@ client.on("message", msg => {
         }
     }
 
+    if (msg.content.includes("/c ")) {
+        log('info', `กำลังรับข้อความ Chinese`);
+
+        let text = msg.content.split(" ")
+        if (text.length == 0) {
+            msg.reply('กรุณาพิมพ์ให้ถูกอีสัส');
+        } else {
+            text.shift()
+            const broadcast = client.voice.createBroadcast();
+            var channelId = msg.member.voice.channelID;
+            var channel = client.channels.cache.get(channelId);
+            channel.join().then(connection => {
+                broadcast.play(discordTTS.getVoiceStream(text.join(" "), 'zh-HK'), {
+                    volume: 0.2,
+                });
+                const dispatcher = connection.play(broadcast);
+                msg.reply('เดี๋ยวอั๋วพูดให้นะจ๊ะ');
+                log('info', `Speaking: ${text.join(" ")}`);
+                return;
+            }).catch(err => {
+                log('error', `Description: ${err}`);
+            });
+        }
+    }
+
+    if (msg.content.includes("/k ")) {
+        log('info', `กำลังรับข้อความ KOREAN`);
+
+        let text = msg.content.split(" ")
+        if (text.length == 0) {
+            msg.reply('กรุณาพิมพ์ให้ถูกอีสัส');
+        } else {
+            text.shift()
+            const broadcast = client.voice.createBroadcast();
+            var channelId = msg.member.voice.channelID;
+            var channel = client.channels.cache.get(channelId);
+            channel.join().then(connection => {
+                broadcast.play(discordTTS.getVoiceStream(text.join(" "), 'ko-KR'), {
+                    volume: 0.2,
+                });
+                const dispatcher = connection.play(broadcast);
+                msg.reply('내가 당신을 위해 그것을 말할 것입니다.');
+                log('info', `Speaking: ${text.join(" ")}`);
+                return;
+            }).catch(err => {
+                log('error', `Description: ${err}`);
+            });
+        }
+    }
+
+    if (msg.content.includes("/k ")) {
+        log('info', `กำลังรับข้อความ KOREAN`);
+
+        let text = msg.content.split(" ")
+        if (text.length == 0) {
+            msg.reply('กรุณาพิมพ์ให้ถูกอีสัส');
+        } else {
+            text.shift()
+            const broadcast = client.voice.createBroadcast();
+            var channelId = msg.member.voice.channelID;
+            var channel = client.channels.cache.get(channelId);
+            channel.join().then(connection => {
+                broadcast.play(discordTTS.getVoiceStream(text.join(" "), 'ko-KR'), {
+                    volume: 0.2,
+                });
+                const dispatcher = connection.play(broadcast);
+                msg.reply('내가 당신을 위해 그것을 말할 것입니다.');
+                log('info', `Speaking: ${text.join(" ")}`);
+                return;
+            }).catch(err => {
+                log('error', `Description: ${err}`);
+            });
+        }
+    }
+
     if (msg.content.includes("/s ")) {
+        log('info', `กำลังรับข้อความ ENGLISH`);
+
         let text = msg.content.split(" ")
         if (text.length == 0) {
             msg.reply('กรุณาพิมพ์ให้ถูกอีสัส');
@@ -75,7 +178,7 @@ client.on("message", msg => {
     }
 
     if (msg.content.includes("/helpSpeak")) {
-        msg.channel.send('Slowpoke จะเป็นปากให้พวกมึงเองวิธีใช้งาน ```/พ``` สำหรับภาษาไทย ```/s``` สำหรับภาษาอังกฤษ')
+        msg.channel.send('Slowpoke จะเป็นปากให้พวกมึงเองวิธีใช้งาน ```/พ``` สำหรับภาษาไทย ```/s``` สำหรับภาษาอังกฤษ```/j``` สำหรับภาษาญี่ปุ่น ```/k``` สำหรับภาษาเกาหลี ```/c``` สำหรับภาษาจีน')
     }
 
     if (msg.content === "อี๊") {
